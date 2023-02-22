239ffda6ee9cd7bca8a91e4b0506dd8a0ba62f7f
Kronie <nghiant365@gmail.com>
2023-02-22 21:18:58 +0700

    Init

---
+++ aspnet-core/src/VinaCent.Blaze.EntityFrameworkCore/EntityFrameworkCore/BlazeDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Abp.Zero.EntityFrameworkCore;
+using VinaCent.Blaze.Authorization.Roles;
+using VinaCent.Blaze.Authorization.Users;
+using VinaCent.Blaze.MultiTenancy;
+using VinaCent.Blaze.AppCore.FileUnits;
+using VinaCent.Blaze.AppCore.TextTemplates;
+using VinaCent.Blaze.AppCore.CommonDatas;
+
+namespace VinaCent.Blaze.EntityFrameworkCore
+{
+    // dotnet ef migrations add "Update.Product.v230121082022" -p ./src/VinaCent.Blaze.EntityFrameworkCore -s ./src/VinaCent.Blaze.Web.Mvc --context BlazeDbContext
+    // dotnet ef database update -p ./src/VinaCent.Blaze.EntityFrameworkCore -s ./src/VinaCent.Blaze.Web.Mvc --context BlazeDbContext
+    // dotnet ef migrations remove -p ./src/VinaCent.Blaze.EntityFrameworkCore -s ./src/VinaCent.Blaze.Web.Mvc --context BlazeDbContext
+    public class BlazeDbContext : AbpZeroDbContext<Tenant, Role, User, BlazeDbContext>
+    {
+        /* Define a DbSet for each entity of the application */
+        public DbSet<FileUnit> FileUnits { get; set; }
+
+        public DbSet<TextTemplate> TextTemplates { get; set; }
+
+        public DbSet<CommonData> CommonDatas { get; set; }
+
+        public BlazeDbContext(DbContextOptions<BlazeDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ aspnet-core/src/VinaCent.Blaze.Web.Mvc/Startup/BlazeWebMvcModule.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Abp.Modules;
+using Abp.Reflection.Extensions;
+using VinaCent.Blaze.Configuration;
+using VinaCent.Blaze.Web.Areas.AdminCP.Common;
+
+namespace VinaCent.Blaze.Web.Startup
+{
+    [DependsOn(typeof(BlazeWebCoreModule))]
+    public class BlazeWebMvcModule : AbpModule
+    {
+        private readonly IWebHostEnvironment _env;
+        private readonly IConfigurationRoot _appConfiguration;
+
+        public BlazeWebMvcModule(IWebHostEnvironment env)
+        {
+            _env = env;
+            _appConfiguration = env.GetAppConfiguration();
+        }
+
+        public override void PreInitialize()
+        {
+            Configuration.Navigation.Providers.Add<AdminCpNavigationProvider>();
+        }
+
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(typeof(BlazeWebMvcModule).GetAssembly());
+        }
+    }
+}
